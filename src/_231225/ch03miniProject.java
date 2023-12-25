032c2ac7655b4ef6194bfdcb85c1813195fd1ff9
kimseongjae <stwin755@gmail.com>
2023-12-25 18:22:06 +0900

    ch03 miniProject

---
+++ src/_231225/ch03miniProject.java
@@ -0,0 +1,27 @@
+package _231225;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * 숫자 추측 게임<p>
+ * 1. 사용자가 답을 제시<p>
+ * 2. 프로그램은 자신이 저장한 정수와 비교<p>
+ * 3. 제시된 정수가 더 높은지 낮은지 출력<p>
+ * 4. 게임이 끝나면 몇 번 만에 맞혔는지 출력
+ */
+
+public class ch03miniProject {
+    public static void main(String[] args) {
+        // 1.
+        System.out.print("1부터 100까지 정수 중 하나를 입력하시오 : ");
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+
+        // 2.
+        Random random = new Random();
+
+        // fail
+
+    }
+}
