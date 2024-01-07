51322b8705dcbf717ae0e74c2d61b71e41a850e9
kimseongjae <stwin755@gmail.com>
2024-01-07 16:00:28 +0900

    2588

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); // 1
+        int b = sc.nextInt(); // 2
+
+        int x = b / 100; // 100의 자리 수
+        int y = (b - (x * 100)) / 10; // 10의 자리 수
+        int z = b - ((x * 100) + (y * 10)); // 1의 자리 수
+
+        System.out.println(a * z); // 3
+        System.out.println(a * y); // 4
+        System.out.println(a * x); // 5
+        System.out.println(a * b); // 6
     }
 }
