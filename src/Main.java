cbef3d2ae5ff40a291a9b1900798c995836d48b2
kimseongjae <stwin755@gmail.com>
2024-01-09 22:50:21 +0900

    11382v2

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,11 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String s = br.readLine();
-            String[] num = s.split(" ");
-            long sum = 0;
-            for (int i = 0; i < num.length; i++) {
-                if (isNumeric(num[i])) { // 입력값이 숫자인지 확인
-                    long a = Long.parseLong(num[i]);
-                    sum = sum + a;
-                } else {
-                    System.out.println("숫자 형식이 아닌 입력이 감지되었습니다: " + num[i]);
-                }
-            }
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-            bw.write(sum + "\n");
-            bw.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // 입력값이 숫자로만 이루어져 있는지 확인하는 메서드
-    public static boolean isNumeric(String str) {
-        return str.matches("-?\\d+(\\.\\d+)?");
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        System.out.println(a + b + c);
     }
 }
