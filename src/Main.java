d5b939bed0bc25cb4dfb2fc428f1169b2e56e4c5
kimseongjae <stwin755@gmail.com>
2024-01-09 22:46:09 +0900

    11382

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(); // 1
-        int b = sc.nextInt(); // 2
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String s = br.readLine();
+            String[] num = s.split(" ");
+            long sum = 0;
+            for (int i = 0; i < num.length; i++) {
+                if (isNumeric(num[i])) { // 입력값이 숫자인지 확인
+                    long a = Long.parseLong(num[i]);
+                    sum = sum + a;
+                } else {
+                    System.out.println("숫자 형식이 아닌 입력이 감지되었습니다: " + num[i]);
+                }
+            }
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            bw.write(sum + "\n");
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        int x = b / 100; // 100의 자리 수
-        int y = (b - (x * 100)) / 10; // 10의 자리 수
-        int z = b - ((x * 100) + (y * 10)); // 1의 자리 수
-
-        System.out.println(a * z); // 3
-        System.out.println(a * y); // 4
-        System.out.println(a * x); // 5
-        System.out.println(a * b); // 6
+    // 입력값이 숫자로만 이루어져 있는지 확인하는 메서드
+    public static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");
     }
 }
