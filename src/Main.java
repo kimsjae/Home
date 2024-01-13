e0392d2a41c5d6e210fafc803dfef498203255d2
kimseongjae <stwin755@gmail.com>
2024-01-13 18:57:52 +0900

    9498

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
-        long c = sc.nextLong();
-        System.out.println(a + b + c);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            int score = Integer.parseInt(br.readLine());
+
+            if (score < 0) {
+                bw.write("점수는 0보다 크거나 같고, 100보다 작거나 같은 정수입니다.");
+                bw.flush();
+            }
+            if (score > 100) {
+                bw.write("점수는 0보다 크거나 같고, 100보다 작거나 같은 정수입니다.");
+                bw.flush();
+            }
+
+            if (score <= 100 && score >= 90) {
+                bw.write("A\n");
+            } else if (score < 90 && score >= 80) {
+                bw.write("B\n");
+            } else if (score < 80 && score >= 70) {
+                bw.write("C\n");
+            } else if (score < 70 && score >= 60) {
+                bw.write("D\n");
+            } else {
+                bw.write("F\n");
+            }
+            bw.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
