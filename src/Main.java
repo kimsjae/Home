ac34ba7a9e4ecd0068a4ce73c01c91cfc71ee4ce
kimseongjae <stwin755@gmail.com>
2024-01-13 19:06:55 +0900

    2753

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         try {
-            int score = Integer.parseInt(br.readLine());
-
-            if (score < 0) {
-                bw.write("점수는 0보다 크거나 같고, 100보다 작거나 같은 정수입니다.");
-                bw.flush();
-            }
-            if (score > 100) {
-                bw.write("점수는 0보다 크거나 같고, 100보다 작거나 같은 정수입니다.");
-                bw.flush();
-            }
-
-            if (score <= 100 && score >= 90) {
-                bw.write("A\n");
-            } else if (score < 90 && score >= 80) {
-                bw.write("B\n");
-            } else if (score < 80 && score >= 70) {
-                bw.write("C\n");
-            } else if (score < 70 && score >= 60) {
-                bw.write("D\n");
+            int year = Integer.parseInt(br.readLine());
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+                bw.write("1\n");
             } else {
-                bw.write("F\n");
+                bw.write("0\n");
             }
             bw.flush();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
